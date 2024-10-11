4e46108d6f5b815b02bd0f822ef95957663bab38
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2024-10-10 21:40:57 -0300

    Arreglos en estado de rastreo, como el id debe ser autogenerado, hago los cambios necesarios para el agregar, despues hago el modificar

--- src/main/java/com/example/envios_bios/repositorio/IRepositorioEstadoRastreo.java
+++ src/main/java/com/example/envios_bios/repositorio/IRepositorioEstadoRastreo.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.example.envios_bios.dominio.EstadoRastreo;
 
-public interface IRepositorioEstadoRastreo extends JpaRepository<EstadoRastreo,Integer>{
-    
+public interface IRepositorioEstadoRastreo extends JpaRepository<EstadoRastreo, Integer> {
+
     // Paginación y búsqueda por idRastreo (como string) o por descripcion
-/* 
-    @Query("SELECT r FROM estadoRastreo r WHERE cast(r.idRastreo as string) = ?1 OR r.descripcion LIKE concat('%', ?1, '%')")
+    @Query("SELECT r FROM EstadoRastreo r WHERE cast(r.idRastreo as string) = ?1 OR r.descripcion LIKE concat('%', ?1, '%')")
     Page<EstadoRastreo> buscarPagina(String criterio, Pageable pageable);
-    */
+
+    EstadoRastreo findByDescripcion(String descripcion);
 }
