68aee8ccdcd8528fbb3ae9ec62634e3c6754baf7
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2024-10-23 20:16:55 -0300

    Paginación en Sucursales OK

--- src/main/java/com/example/envios_bios/repositorio/IRepositorioSucursal.java
+++ src/main/java/com/example/envios_bios/repositorio/IRepositorioSucursal.java
@@ -2,6 +2,8 @@ package com.example.envios_bios.repositorio;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,5 +12,7 @@ import com.example.envios_bios.dominio.Sucursal;
 public interface IRepositorioSucursal extends JpaRepository<Sucursal, Long>, JpaSpecificationExecutor<Sucursal> {
 
     List<Sucursal> findByNombreContaining(String criterio);
+
+    Page<Sucursal> findByNombreContaining(String criterio, Pageable pageable);
 
 }
--- src/main/java/com/example/envios_bios/servicios/IServicioSucursales.java
+++ src/main/java/com/example/envios_bios/servicios/IServicioSucursales.java
@@ -2,15 +2,26 @@ package com.example.envios_bios.servicios;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.example.envios_bios.dominio.Sucursal;
 import com.example.envios_bios.excepciones.ExcepcionEnviosBios;
 
 public interface IServicioSucursales {
-    
+
     List<Sucursal> listar();
-    List<Sucursal> buscar(String criterio);
+
+    Page<Sucursal> listarPaginacion(Pageable pageable);
+
+    Page<Sucursal> buscar(String criterio, Pageable pageable);
+
+    Page<Sucursal> buscarPaginado(String criterio, Pageable pageable);
+
     Sucursal obtener(Long numero);
+
     void agregar(Sucursal sucursal) throws ExcepcionEnviosBios;
+
     void modificar(Sucursal sucursal) throws ExcepcionEnviosBios;
+
     void eliminar(Long numero) throws ExcepcionEnviosBios;
 }
